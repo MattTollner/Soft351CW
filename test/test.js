3ae588e3829461f4ea04544fb9570bbce1992903
Matthew Tollner <matt.tollner@live.co.uk>
2018-01-04 17:24:17 +0000

    Code Cleanup

--- test/test.js
+++ test/test.js
@@ -1,14 +1,13 @@
 
 var webdriver = require('selenium-webdriver');
-var browser = new webdriver.Builder().usingServer().withCapabilities({'browserName': 'chrome' }).build();
- 
+var browser = new webdriver.Builder().usingServer().withCapabilities({browserName: 'chrome' }).build();
+
 browser.get('http://localhost:5000');
 
-browser.findElement(webdriver.By.id("userInput")).sendKeys("NateDog");
-browser.findElement(webdriver.By.id("joinLobby")).click();
+browser.findElement(webdriver.By.id('userInput')).sendKeys('NateDog');
+browser.findElement(webdriver.By.id('joinLobby')).click();
 
 
-
-//var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
+// var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
 
 // driver.get('https://localhost:5000');
