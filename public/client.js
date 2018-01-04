e9adfdfc699bfeb364872eebbad840e61bea5783
Matthew Tollner <matt.tollner@live.co.uk>
2018-01-04 22:27:49 +0000

    Revert shooting not working

--- public/client.js
+++ public/client.js
@@ -96,16 +96,6 @@ $(document).ready(function () {
             new Player(data.player[i]);
         }
 
-        for (var i = 0; i < data.bullet.length; i++) {
-            new Bullet(data.bullet[i]);
-        }
-
-
-        $('#playerList').empty();
-        for (i in Player.list) {
-            $('#playerList').append('<li class="playerListItem">' + Player.list[i].username + '</li>');
-        }
-
     });
 
     socket.on('updatePlayer', function (data) {
@@ -117,18 +107,6 @@ $(document).ready(function () {
             if (player) {
                 if (updatedP.x !== undefined) { player.x = updatedP.x; }
                 if (updatedP.y !== undefined) { player.y = updatedP.y; }
-                if (updatedP.lives !== undefined) { player.lives = updatedP.lives; }
-                if (updatedP.score !== undefined) { player.score = updatedP.score; }
-                if (updatedP.ammo !== undefined) { player.ammo = updatedP.ammo; }
-            }
-        }
-
-        for (var i = 0; i < data.bullet.length; i++) {
-            var updatedB = data.bullet[i];
-            var bullet = Bullet.list[data.bullet[i].id];
-            if (bullet) {
-                if (updatedB.x !== undefined) { bullet.x = updatedB.x; }
-                if (updatedB.y !== undefined) { bullet.y = updatedB.y; }
             }
         }
     });
@@ -136,10 +114,6 @@ $(document).ready(function () {
     socket.on('removePlayer', function (data) {
         for (var i = 0; i < data.player.length; i++) {
             delete Player.list[data.player[i]];
-        }
-
-        for (var i = 0; i < data.bullet.length; i++) {
-            delete Bullet.list[data.bullet[i]];
         }
     });
 
@@ -176,26 +150,6 @@ $(document).ready(function () {
         else if (event.keyCode === 87) { socket.emit('keyPress', { inputId: 'up', state: false }); }
     }
 
-    //Handle Mouse Press
-    document.onmousedown = function (event) {
-        socket.emit('keyPress', { inputId: 'leftMouse', state: true });
-    }
-
-    document.onmouseup = function (event) {
-        socket.emit('keyPress', { inputId: 'leftMouse', state: false });
-    }
-
-    document.onmousemove = function (event) {
-        var angle = [];
-
-        angle = {
-            x: event.clientX,
-            y: event.clientY,
-        }
-
-        socket.emit('keyPress', { inputId: 'mouseAngle', state: angle });
-    }
-
 
 });
 
@@ -207,10 +161,6 @@ setInterval(function () {
     }
     for (var i in Player.list) {
         Player.list[i].draw();
-    }
-
-    for (var i in Bullet.list) {
-        Bullet.list[i].draw();
     }
 
 }, 1000 / 30);
@@ -232,34 +182,15 @@ var Player = function (playerInfo) {
     self.username = playerInfo.uname;
     self.x = playerInfo.x;
     self.y = playerInfo.y;
-    self.lives = playerInfo.lives;
-    self.ammo = playerInfo.ammo;
-    self.score = playerInfo.score
+    self.hp = playerInfo.hp;
     Player.list[self.id] = self;
 
     self.draw = function () {
         ctx.fillStyle = 'green';
-        ctx.fillRect(self.x - 5, self.y - 5, 10, 10);
+        ctx.fillRect(self.x - 5, self.y - 5, 5, 5);
     }
 
     return self;
 }
 
 Player.list = {};
-
-
-var Bullet = function (bulletInfo) {
-    var self = {};
-    self.id = bulletInfo.id;
-    self.x = bulletInfo.x;
-    self.y = bulletInfo.y;
-    Bullet.list[self.id] = self;
-
-    self.draw = function () {
-        ctx.fillStyle = 'red';
-        ctx.fillRect(self.x - 5, self.y - 5, 3, 3);
-    }
-    return self;
-}
-
-Bullet.list = {};
