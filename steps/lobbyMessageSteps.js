097575cdb3fca6d107ce8ccdc18a6d398d250114
Matthew Tollner <matt.tollner@live.co.uk>
2018-01-04 23:36:40 +0000

    Potentail Ficx

--- steps/lobbyMessageSteps.js
+++ steps/lobbyMessageSteps.js
@@ -5,13 +5,13 @@ module.exports = function () {
     this.Given(/^I have logged in$/, function (callback) {
         // Write code here that turns the phrase above into concrete actions
         helpers.loadPage("http://localhost:5000").then(function () {
-            driver.findElement(By.id("userInput")).sendKeys("Nate").then(function () {
-                driver.findElement(By.id("joinLobby")).click().then(function (result) {
+            driver.findElement(By.id("userInput")).sendKeys("Nate~").then(function () {
+                
                     var element = driver.findElement(By.id('loginText'));
                     element.getText().then(s => assert.equal(s, "")).then(function () {
                         callback();
+                  
                     });
-                });
             });
         })
     });
